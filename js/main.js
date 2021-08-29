472fd0667160a74c51ba82795594ea0eb32cb546
ranzgit <likingyo@gmail.com>
2021-08-29 13:19:42 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus(); // 돋보기를 클릭하면 포커스 적용
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색'); // html요소를 지정
-});
-
-searchInputEl.addEventListener('blur', function () {  // focus가 해제
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', ''); // html요소를 지정
-});
-
 // 배지고정제어과 ScrollToTop
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -162,9 +145,5 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-
-// 올해 연도 자동계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
 
 
